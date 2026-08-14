42d95994a4c522ec0cfca1109bba14a6b3efc0fa
thongtr-dev <thongtr.dev@gmail.com>
2026-08-14 18:31:56 +0700

    feat: Add curriculum for studying mlsys

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,21 +5,18 @@ import './globals.css';
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
-  weight: ['300', '400', '500', '600'],
   display: 'swap',
 });
 
 const newsreader = Newsreader({
   variable: '--font-newsreader',
   subsets: ['latin'],
-  weight: ['400', '500', '700'],
   display: 'swap',
 });
 
 const jetbrainsMono = JetBrains_Mono({
   variable: '--font-jetbrains-mono',
   subsets: ['latin'],
-  weight: ['400', '500', '700'],
   display: 'swap',
 });
 
